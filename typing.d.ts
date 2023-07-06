ae5db688df501a62959ffce364175741914edf4d
ritesh <076bct033.@riteshsagarmatha>
2023-07-06 15:25:21 +0545

    Frontend final commit with nextjs

---
+++ typing.d.ts
@@ -0,0 +1,10 @@
+type Product ={
+    id:number;
+    title:string;
+    price:number;
+    description:string;
+    category:string;
+    image:string;
+    ratings:{rate:number;count:number};
+
+};
